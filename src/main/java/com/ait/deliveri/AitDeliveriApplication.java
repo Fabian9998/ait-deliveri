125f595ebbcef3ad75d78ee05d9995d505534103
Fabian Minor Romero <admifabio9998@gmail.com>
2026-03-27 12:18:59 -0600

    FIX -> Configuracion inicial del proyecto

--- src/main/java/com/ait/deliveri/AitDeliveriApplication.java
+++ src/main/java/com/ait/deliveri/AitDeliveriApplication.java
@@ -1,12 +1,17 @@
 package com.ait.deliveri;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AitDeliveriApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT-6"));
 		SpringApplication.run(AitDeliveriApplication.class, args);
 	}
 

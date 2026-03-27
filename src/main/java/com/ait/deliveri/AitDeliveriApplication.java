2d04365e4724c08e8c0fbc28824c508a5d619493
Fabian Minor Romero <admifabio9998@gmail.com>
2026-03-27 11:55:28 -0600

    Initial commit

---
+++ src/main/java/com/ait/deliveri/AitDeliveriApplication.java
@@ -0,0 +1,13 @@
+package com.ait.deliveri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitDeliveriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AitDeliveriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ait/deliveri/AitDeliveriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ait.deliveri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitDeliveriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

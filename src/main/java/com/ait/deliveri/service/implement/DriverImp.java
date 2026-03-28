2409036b3519c8470ee979c3c002e6402cc1659d
Fabian Minor Romero <admifabio9998@gmail.com>
2026-03-28 00:29:50 -0600

    FIX(ControllerAdvice) -> Se ajustan mensajes controlados

--- src/main/java/com/ait/deliveri/service/implement/DriverImp.java
+++ src/main/java/com/ait/deliveri/service/implement/DriverImp.java
@@ -18,6 +18,8 @@ import com.ait.deliveri.db.dto.DriverRequest;
 import com.ait.deliveri.db.dto.DriverResponse;
 import com.ait.deliveri.db.entity.Driver;
 import com.ait.deliveri.db.repository.imp.DriverRepository;
+import com.ait.deliveri.exception.BadRequestException;
+import com.ait.deliveri.exception.NotFoundException;
 import com.ait.deliveri.service.IDriverService;
 
 import lombok.RequiredArgsConstructor;
@@ -31,79 +33,56 @@ public class DriverImp implements IDriverService {
 	@Override
 	@Transactional(readOnly = true)
 	public ResponseEntity<?> get(Map<String, String> params, Pageable pageable) {
-		try {
-            pageable = "*".equals(params.get("size")) ? PageRequest.of(0, Integer.MAX_VALUE, pageable.getSort())
-                    : pageable;
-            Page<Driver> page = repository.findAll(params, pageable);
+		pageable = "*".equals(params.get("size")) ? PageRequest.of(0, Integer.MAX_VALUE, pageable.getSort()) : pageable;
+		Page<Driver> page = repository.findAll(params, pageable);
 
-            if (page.getContent().isEmpty()) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(Map.of("codigo", 404, "mensaje", "No encontrado"));
-            }
+		if (page.getContent().isEmpty()) {
+			throw new NotFoundException("No encontrado");
+		}
 
-            List<DriverResponse> drivers = page.getContent().stream().map(this::mapToResponse)
-                    .collect(Collectors.toList());
+		List<DriverResponse> drivers = page.getContent().stream().map(this::mapToResponse).collect(Collectors.toList());
 
-            Map<String, Object> data = Map.of(
-                    "data", drivers,
-                    "paginas", page.getTotalPages(),
-                    "total", page.getTotalElements());
+		Map<String, Object> data = Map.of(
+				"data", drivers, 
+				"paginas", page.getTotalPages(), 
+				"total", page.getTotalElements());
 
-            return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", data));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.internalServerError()
-                    .body(Map.of("codigo", 500, "mensaje", "Ocurrio un error en el sistema, intentelo mas tarde."));
-        }
+		return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", data));
 	}
 
 	@Override
 	@Transactional
 	public ResponseEntity<?> create(DriverRequest request) {
-		try {
-			Boolean license = repository.existsByLicenseNumber(request.getLicenseNumber());
-			if(license) {
-				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(Map.of("codigo", 400, "mensaje", "El numero de licencia ya se encuetra reguistrado"));
-			}
-			
-			Driver entity = new Driver();
-			entity.setName(request.getName());
-			entity.setLicenseNumber(request.getLicenseNumber());
-			entity.setActive(true);
-			entity = repository.save(entity);
-			
-			DriverResponse reponse = this.mapToResponse(entity);
-			return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(Map.of("codigo", 201, "mensaje", "Recurso creado", "data", reponse));
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.internalServerError()
-                    .body(Map.of("codigo", 500, "mensaje", "Ocurrio un error en el sistema, intentelo mas tarde."));
+		Boolean license = repository.existsByLicenseNumber(request.getLicenseNumber());
+		if (license) {
+			throw new BadRequestException("El numero de licencia ya se encuentra registrado");
 		}
+
+		Driver entity = new Driver();
+		entity.setName(request.getName());
+		entity.setLicenseNumber(request.getLicenseNumber());
+		entity.setActive(true);
+		entity = repository.save(entity);
+
+		DriverResponse reponse = this.mapToResponse(entity);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(Map.of("codigo", 201, "mensaje", "Recurso creado", "data", reponse));
 	}
 
 	@Override
 	@Transactional
 	public ResponseEntity<?> status(UUID id) {
-		try {
-			Optional<Driver> opt = repository.findById(id);
-			if(opt.isEmpty()) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(Map.of("codigo", 404, "mensaje", "No encontrado"));
-			}
-			
-			Driver entity = opt.get();
-			entity.setActive(!entity.getActive());
-			entity = repository.save(entity);
-			
-			DriverResponse reponse = this.mapToResponse(entity);
-			return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", reponse));
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.internalServerError()
-                    .body(Map.of("codigo", 500, "mensaje", "Ocurrio un error en el sistema, intentelo mas tarde."));
+		Optional<Driver> opt = repository.findById(id);
+		if (opt.isEmpty()) {
+			throw new NotFoundException("No encontrado");
 		}
+
+		Driver entity = opt.get();
+		entity.setActive(!entity.getActive());
+		entity = repository.save(entity);
+
+		DriverResponse reponse = this.mapToResponse(entity);
+		return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", reponse));
 	}
 	
 	private DriverResponse mapToResponse(Driver entity) {
--- src/main/java/com/ait/deliveri/service/implement/OrderImp.java
+++ src/main/java/com/ait/deliveri/service/implement/OrderImp.java
@@ -23,6 +23,8 @@ import com.ait.deliveri.db.entity.OrderStatus;
 import com.ait.deliveri.db.repository.imp.DriverRepository;
 import com.ait.deliveri.db.repository.imp.OrderRepository;
 import com.ait.deliveri.db.repository.imp.OrderStatusRepository;
+import com.ait.deliveri.exception.BadRequestException;
+import com.ait.deliveri.exception.NotFoundException;
 import com.ait.deliveri.service.IOrderService;
 import com.ait.deliveri.utils.FileUtils;
 
@@ -39,163 +41,123 @@ public class OrderImp implements IOrderService {
 	@Override
 	@Transactional(readOnly = true)
 	public ResponseEntity<?> get(Map<String, String> params, Pageable pageable) {
-		try {
-            pageable = "*".equals(params.get("size")) ? PageRequest.of(0, Integer.MAX_VALUE, pageable.getSort())
-                    : pageable;
-            Page<Order> page = repository.findAll(params, pageable);
+		pageable = "*".equals(params.get("size")) ? PageRequest.of(0, Integer.MAX_VALUE, pageable.getSort()) : pageable;
+		Page<Order> page = repository.findAll(params, pageable);
 
-            if (page.getContent().isEmpty()) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(Map.of("codigo", 404, "mensaje", "No encontrado"));
-            }
+		if (page.getContent().isEmpty()) {
+			throw new NotFoundException("No encontrado");
+		}
 
-            List<OrderResponse> orders = page.getContent().stream().map(this::mapToResponse)
-                    .collect(Collectors.toList());
+		List<OrderResponse> orders = page.getContent().stream().map(this::mapToResponse).collect(Collectors.toList());
 
-            Map<String, Object> data = Map.of(
-                    "data", orders,
-                    "paginas", page.getTotalPages(),
-                    "total", page.getTotalElements());
+		Map<String, Object> data = Map.of(
+				"data", orders, 
+				"paginas", page.getTotalPages(), 
+				"total", page.getTotalElements());
 
-            return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", data));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.internalServerError()
-                    .body(Map.of("codigo", 500, "mensaje", "Ocurrio un error en el sistema, intentelo mas tarde."));
-        }
+		return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", data));
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public ResponseEntity<?> getById(UUID id) {
-		try {
-			Optional<Order> opt = repository.findById(id);
-			if(opt.isEmpty()) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(Map.of("codigo", 404, "mensaje", "No encontrado"));
-			}
-			
-			OrderResponse response = this.mapToResponse(opt.get());
-			return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", response));
-		} catch (Exception e) {
-			e.printStackTrace();
-            return ResponseEntity.internalServerError()
-                    .body(Map.of("codigo", 500, "mensaje", "Ocurrio un error en el sistema, intentelo mas tarde."));
+		Optional<Order> opt = repository.findById(id);
+		if (opt.isEmpty()) {
+			throw new NotFoundException("No encontrado");
 		}
+
+		OrderResponse response = this.mapToResponse(opt.get());
+		return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", response));
+
 	}
 
 	@Override
 	@Transactional
 	public ResponseEntity<?> create(OrderRequest request) {
-		try {
-			Optional<OrderStatus> statusOpt = statusRepository.findByCode("CREATED");
-			if(statusOpt.isEmpty()) {
-				return null;
-			}
-			
-			Order entity = Order.builder()
-					.status(statusOpt.get())
-					.origin(request.getOrigin())
-					.destination(request.getDestination())
-					.build();
-			entity = repository.save(entity);
-			
-			OrderResponse response = this.mapToResponse(entity);
-			return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(Map.of("codigo", 201, "mensaje", "Recurso creado", "data", response));
-		} catch (Exception e) {
-			e.printStackTrace();
-            return ResponseEntity.internalServerError()
-                    .body(Map.of("codigo", 500, "mensaje", "Ocurrio un error en el sistema, intentelo mas tarde."));
+		Optional<OrderStatus> statusOpt = statusRepository.findByCode("CREATED");
+		if (statusOpt.isEmpty()) {
+			throw new NotFoundException("No se encontro recurso");
 		}
+
+		Order entity = Order.builder()
+				.status(statusOpt.get())
+				.origin(request.getOrigin())
+				.destination(request.getDestination())
+				.build();
+		entity = repository.save(entity);
+
+		OrderResponse response = this.mapToResponse(entity);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(Map.of("codigo", 201, "mensaje", "Recurso creado", "data", response));
 	}
 	
 	@Override
 	@Transactional
 	public ResponseEntity<?> assignDriver(UUID id, UUID driverId, AssignDriverRequest request) {
-		try {
-			Optional<Order> opt = repository.findById(id);
-			if(opt.isEmpty()) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(Map.of("codigo", 404, "mensaje", "No encontrado"));
-			}
-			
-			Order entity = opt.get();
-			if(!entity.getStatus().getCode().equals("CREATED")) {
-				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(Map.of("codigo", 400, "mensaje", "La orden debe estar en estatus creada para asignar a un conductor"));
-			}
-			
-			Optional<Driver> optDrivert = driverRepository.findById(driverId);
-			if(optDrivert.isEmpty()) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(Map.of("codigo", 404, "mensaje", "No se encontro el conductor"));
-			}
-			
-			if(!optDrivert.get().getActive()) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(Map.of("codigo", 404, "mensaje", "No se puede asignar un conductor inhabilitado"));
-			}
-			
-			String pdfPath = FileUtils.saveBase64File(entity.getId().toString(), "file" + entity.getId(), request.getPdf());
-			String imagePath = FileUtils.saveBase64File(entity.getId().toString(), "img" + entity.getId(), request.getImage());
-			
-			entity.setDriver(optDrivert.get());
-			entity.setPdf(pdfPath);
-			entity.setImage(imagePath);
-			entity = repository.save(entity);
-			
-			OrderResponse response = this.mapToResponse(entity);
-			return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", response));
-		} catch (Exception e) {
-			e.printStackTrace();
-            return ResponseEntity.internalServerError()
-                    .body(Map.of("codigo", 500, "mensaje", "Ocurrio un error en el sistema, intentelo mas tarde."));
+		Optional<Order> opt = repository.findById(id);
+		if (opt.isEmpty()) {
+			throw new NotFoundException("No encontrado");
 		}
+
+		Order entity = opt.get();
+		if (!entity.getStatus().getCode().equals("CREATED")) {
+			throw new BadRequestException("La orden debe estar en estatus creada para asignar a un conductor");
+		}
+
+		Optional<Driver> optDrivert = driverRepository.findById(driverId);
+		if (optDrivert.isEmpty()) {
+			throw new NotFoundException("No se encontro el conductor");
+		}
+
+		if (!optDrivert.get().getActive()) {
+			throw new BadRequestException("No se puede asignar un conductor inhabilitado");
+		}
+
+		String pdfPath = FileUtils.saveBase64File(entity.getId().toString(), "file" + entity.getId(), request.getPdf());
+		String imagePath = FileUtils.saveBase64File(entity.getId().toString(), "img" + entity.getId(),
+				request.getImage());
+
+		entity.setDriver(optDrivert.get());
+		entity.setPdf(pdfPath);
+		entity.setImage(imagePath);
+		entity = repository.save(entity);
+
+		OrderResponse response = this.mapToResponse(entity);
+		return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", response));
 	}
 
 	@Override
 	@Transactional
 	public ResponseEntity<?> status(UUID id, String status) {
-		try {
-			Optional<Order> opt = repository.findById(id);
-			if(opt.isEmpty()) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(Map.of("codigo", 404, "mensaje", "No encontrado"));
-			}
-			
-			Order entity = opt.get();
-			if(entity.getStatus().getCode().equals("DELIVERED") || entity.getStatus().getCode().equals("CANCELLED")) {
-				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(Map.of("codigo", 400, "mensaje", "No se puede cambiar el estatus a una orden entregada o cancelada"));
-			}
-			
-			if(entity.getStatus().getCode().equals("IN_TRANSIT") && status.equals("CREATED")) {
-				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-						.body(Map.of("codigo", 400, "mensaje", "No se puede cambiar el estatus a creada a una orden en proceso de entrega"));
-			}
-			
-			if(entity.getStatus().getCode().equals("CREATED") && status.equals("IN_TRANSIT") && entity.getDriver() == null) {
-				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-						.body(Map.of("codigo", 400, "mensaje", "Debe asignar un condutor para poner la orden en estatus de entrega"));
-			}
-			
-			Optional<OrderStatus> optStatus = statusRepository.findByCode(status);
-			if(optStatus.isEmpty()) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(Map.of("codigo", 404, "mensaje", "No se encontro el recurso"));
-			}
-			
-			entity.setStatus(optStatus.get());
-			entity = repository.save(entity);
-			
-			OrderResponse response = this.mapToResponse(entity);
-			return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", response));
-		} catch (Exception e) {
-			e.printStackTrace();
-            return ResponseEntity.internalServerError()
-                    .body(Map.of("codigo", 500, "mensaje", "Ocurrio un error en el sistema, intentelo mas tarde."));
+		Optional<Order> opt = repository.findById(id);
+		if (opt.isEmpty()) {
+			throw new NotFoundException("No encontrado");
 		}
+
+		Order entity = opt.get();
+		if (entity.getStatus().getCode().equals("DELIVERED") || entity.getStatus().getCode().equals("CANCELLED")) {
+			throw new BadRequestException("No se puede cambiar el estatus a una orden entregada o cancelada");
+		}
+
+		if (entity.getStatus().getCode().equals("IN_TRANSIT") && status.equals("CREATED")) {
+			throw new BadRequestException("No se puede cambiar el estatus a creada a una orden en proceso de entrega");
+		}
+
+		if (entity.getStatus().getCode().equals("CREATED") && status.equals("IN_TRANSIT")
+				&& entity.getDriver() == null) {
+			throw new BadRequestException("Debe asignar un condutor para poner la orden en estatus de entrega");
+		}
+
+		Optional<OrderStatus> optStatus = statusRepository.findByCode(status);
+		if (optStatus.isEmpty()) {
+			throw new NotFoundException("No se encontro recurso");
+		}
+
+		entity.setStatus(optStatus.get());
+		entity = repository.save(entity);
+
+		OrderResponse response = this.mapToResponse(entity);
+		return ResponseEntity.ok(Map.of("codigo", 200, "mensaje", "Ok", "data", response));
 	}
 	
 	private OrderResponse mapToResponse(Order entity) {
